182ee132a0ce3b03f9a7c0064157300b1aa9f58f
Will Lovo <will9637@gmail.com>
2024-11-21 23:28:26 -0500

    Add initial Resume Page component

---
+++ src/app/resume-page/resume-page.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {BasePageComponent} from '../base-page/base-page.component';
+
+@Component({
+  selector: 'app-resume-page',
+  standalone: true,
+  imports: [BasePageComponent],
+  templateUrl: './resume-page.component.html',
+})
+export class ResumePageComponent {
+
+}
