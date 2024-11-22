05c528f34a02cf10adeba4489a6413f8ba933091
Will Lovo <will9637@gmail.com>
2024-11-21 23:28:43 -0500

    Update routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {IntroPageComponent} from './intro-page/intro-page.component';
+import {SocialMediaComponent} from './social-media/social-media.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroPageComponent, SocialMediaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {IntroPageComponent} from './intro-page/intro-page.component';
+import {ResumePageComponent} from './resume-page/resume-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'intro', component: IntroPageComponent},
+  {path: 'resume', component: ResumePageComponent},
+];
