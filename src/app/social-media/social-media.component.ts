2928290aa400fbe222459cc23af5c155990e034a
Will Lovo <will9637@gmail.com>
2024-11-21 23:26:34 -0500

    Add incomplete Social Media component

---
+++ src/app/social-media/social-media.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-social-media',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-media.component.html',
+})
+export class SocialMediaComponent {
+
+}
